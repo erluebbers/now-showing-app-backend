57bae1e51bf3fc7b8a3d890d65c30f092c22ab5c
Erik Luebbers <erluebbers@gmail.com>
2022-04-05 05:50:54 -0700

    edit controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add your routes here
   get "/theaters" do
     theaters = Theater.all
     theaters.to_json(include: :movies)
